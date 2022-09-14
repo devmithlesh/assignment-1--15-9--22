0f2f923622e9937c550525fdde6a140e73982b24
Mithlesh Nirmal <mithleshnirmal786123@gmail.com>
2022-09-14 17:02:52 +0530

    hmm

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,21 +3,37 @@ import Axios from "axios";
 import Card from "./Card";
 import "./Main.css";
 function Main() {
-  const [lidtOfCoins, setListOfCoins] = useState([]);
+  const [listOfCoins, setListOfCoins] = useState([]);
+  //search for usestate
+  const [searchWord, setSearchWord] = useState("");
   useEffect(() => {
-    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=24").then(
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
       (response) => {
         //  console.log(Response.data);
         setListOfCoins(response.data.coins);
       }
     );
   }, []);
+  //search coins...
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
 
   return (
     <>
       <div className="container">
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search Bitcoine"
+            className="search-box-i"
+            onChange={(event) => {
+              setSearchWord(event.target.value);
+            }}
+          />
+        </div>
         <div className="div-box">
-          {lidtOfCoins.map((coin) => {
+          {filteredCoins.map((coin) => {
             return (
               <Card
                 name={coin.name}
